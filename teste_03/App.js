4985bf929c146c4538cc67bccad6272d9a4252c7
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-06 08:11:22 -0300

    add teste 3

---
+++ teste_03/App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Index from './src/pages/Index';
+import Dobro from './src/pages/Dobro';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName='Index'>
+      <Stack.Screen name='Index' component={Index} />
+      <Stack.Screen name='Dobro' component={Dobro} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  
+  )
+}
