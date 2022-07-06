15931c201a5683c17d159efe1acd889bafb38d6c
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-06 08:28:20 -0300

    add

---
+++ teste_03/src/pages/Index.js
@@ -0,0 +1,10 @@
+import { Text, View, Button } from "react-native";
+
+export default props => {
+    return (
+        <View>
+            <Text>Tela Inicial</Text>
+                <Button title="Ir para Dobro" onPress={() => { props.navigation.navigate("Dobro"); }} />
+        </View>
+    )
+}
